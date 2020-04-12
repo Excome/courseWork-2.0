9666b2cd04c47afc5ae6b99b6207e0f8c319f119
Excome <56866949+Excome@users.noreply.github.com>
2020-04-12 16:08:08 +0300

    PeriodicalsForm add func

--- courseWork 2.0/PeriodicalsForm.Designer.cs
+++ courseWork 2.0/PeriodicalsForm.Designer.cs
@@ -31,16 +31,29 @@
             this.label1 = new System.Windows.Forms.Label();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.tabPage3 = new System.Windows.Forms.TabPage();
-            this.tabPage4 = new System.Windows.Forms.TabPage();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.number_of_year = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.nameAddTB = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numberOfYearAddTB = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.numberOfYearUpdateTB = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.nameUpdateTB = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -63,12 +76,12 @@
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Controls.Add(this.tabPage3);
             this.tabControl1.Controls.Add(this.tabPage4);
-            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.tabControl1.Location = new System.Drawing.Point(0, 56);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
             this.tabControl1.Size = new System.Drawing.Size(562, 380);
             this.tabControl1.TabIndex = 2;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
             // 
             // tabPage1
             // 
@@ -80,34 +93,6 @@
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Список изданий";
             this.tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(792, 354);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Добавить";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // tabPage3
-            // 
-            this.tabPage3.Location = new System.Drawing.Point(4, 22);
-            this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(792, 354);
-            this.tabPage3.TabIndex = 2;
-            this.tabPage3.Text = "Изменить";
-            this.tabPage3.UseVisualStyleBackColor = true;
-            // 
-            // tabPage4
-            // 
-            this.tabPage4.Location = new System.Drawing.Point(4, 22);
-            this.tabPage4.Name = "tabPage4";
-            this.tabPage4.Size = new System.Drawing.Size(792, 354);
-            this.tabPage4.TabIndex = 3;
-            this.tabPage4.Text = "Удалить";
-            this.tabPage4.UseVisualStyleBackColor = true;
             // 
             // dataGridView1
             // 
@@ -152,6 +137,155 @@
             this.number_of_year.ReadOnly = true;
             this.number_of_year.Width = 105;
             // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.addButton);
+            this.tabPage2.Controls.Add(this.numberOfYearAddTB);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.nameAddTB);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(554, 354);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Добавить";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.comboBox1);
+            this.tabPage3.Controls.Add(this.updateButton);
+            this.tabPage3.Controls.Add(this.numberOfYearUpdateTB);
+            this.tabPage3.Controls.Add(this.label4);
+            this.tabPage3.Controls.Add(this.label5);
+            this.tabPage3.Controls.Add(this.nameUpdateTB);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(554, 354);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Изменить";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(557, 354);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Удалить";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // nameAddTB
+            // 
+            this.nameAddTB.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nameAddTB.Location = new System.Drawing.Point(217, 15);
+            this.nameAddTB.Name = "nameAddTB";
+            this.nameAddTB.Size = new System.Drawing.Size(269, 30);
+            this.nameAddTB.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(54, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Название:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(54, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(281, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Количество номеров в год:";
+            // 
+            // numberOfYearAddTB
+            // 
+            this.numberOfYearAddTB.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numberOfYearAddTB.Location = new System.Drawing.Point(341, 52);
+            this.numberOfYearAddTB.Name = "numberOfYearAddTB";
+            this.numberOfYearAddTB.Size = new System.Drawing.Size(145, 30);
+            this.numberOfYearAddTB.TabIndex = 3;
+            this.numberOfYearAddTB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.numberOfYearAddTB_KeyPress);
+            // 
+            // addButton
+            // 
+            this.addButton.BackColor = System.Drawing.Color.LightBlue;
+            this.addButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.addButton.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.addButton.Location = new System.Drawing.Point(157, 88);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(197, 44);
+            this.addButton.TabIndex = 7;
+            this.addButton.Text = "Добавить";
+            this.addButton.UseVisualStyleBackColor = false;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // updateButton
+            // 
+            this.updateButton.BackColor = System.Drawing.Color.LightBlue;
+            this.updateButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.updateButton.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.updateButton.Location = new System.Drawing.Point(158, 133);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(197, 44);
+            this.updateButton.TabIndex = 12;
+            this.updateButton.Text = "Изменить";
+            this.updateButton.UseVisualStyleBackColor = false;
+            // 
+            // numberOfYearUpdateTB
+            // 
+            this.numberOfYearUpdateTB.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numberOfYearUpdateTB.Location = new System.Drawing.Point(342, 97);
+            this.numberOfYearUpdateTB.Name = "numberOfYearUpdateTB";
+            this.numberOfYearUpdateTB.Size = new System.Drawing.Size(145, 30);
+            this.numberOfYearUpdateTB.TabIndex = 11;
+            this.numberOfYearUpdateTB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.numberOfYearUpdateTB_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(55, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(281, 23);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Количество номеров в год:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(55, 63);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 23);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Название:";
+            // 
+            // nameUpdateTB
+            // 
+            this.nameUpdateTB.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nameUpdateTB.Location = new System.Drawing.Point(218, 60);
+            this.nameUpdateTB.Name = "nameUpdateTB";
+            this.nameUpdateTB.Size = new System.Drawing.Size(269, 30);
+            this.nameUpdateTB.TabIndex = 8;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(59, 23);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(428, 31);
+            this.comboBox1.TabIndex = 13;
+            this.comboBox1.SelectionChangeCommitted += new System.EventHandler(this.comboBox1_SelectionChangeCommitted);
+            // 
             // PeriodicalsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -167,6 +301,10 @@
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -184,5 +322,16 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn id;
         private System.Windows.Forms.DataGridViewTextBoxColumn name;
         private System.Windows.Forms.DataGridViewTextBoxColumn number_of_year;
+        private System.Windows.Forms.TextBox numberOfYearAddTB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox nameAddTB;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.TextBox numberOfYearUpdateTB;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox nameUpdateTB;
     }
 }
--- courseWork 2.0/PeriodicalsForm.cs
+++ courseWork 2.0/PeriodicalsForm.cs
@@ -24,10 +24,10 @@ namespace courseWork_2._0
         private void PeriodicalsForm_Load(object sender, EventArgs e)
         {
             sqlConnection = MainForm.sqlConnection;
-            LoadData();
+            LoadDataTable();
         }
 
-        private void LoadData()
+        private void LoadDataTable()
         {
             if (sqlConnection.State == ConnectionState.Closed)
             {
@@ -52,6 +52,120 @@ namespace courseWork_2._0
             {
                 dataGridView1.Rows.Add(dataElement);
             }
+           
+        }
+
+        private void LoadDataComboBox(ComboBox cb)
+        {
+            if (sqlConnection.State == ConnectionState.Closed)
+            {
+                sqlConnection.Open();
+            }
+            SqlCommand command = new SqlCommand("SELECT [name] FROM Periodicals ORDER BY [id]", sqlConnection);
+            SqlDataAdapter sqlAdapter = new SqlDataAdapter("SELECT * FROM Periodicals ORDER BY[id]", sqlConnection);
+            DataSet comboboxDataSet = new DataSet();
+            sqlAdapter.Fill(comboboxDataSet);
+            DataTable comboboxDataTable = comboboxDataSet.Tables[0];
+            cb.DataSource = comboboxDataTable;
+            cb.DisplayMember = "name";
+            cb.ValueMember = "id";
+            if (sqlConnection.State == ConnectionState.Open)
+            {
+                sqlConnection.Close();
+            }
+
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (tabControl1.SelectedIndex == 0)
+            {
+                dataGridView1.Rows.Clear();
+                LoadDataTable();
+            }
+            if (tabControl1.SelectedIndex == 1)
+            {
+                nameAddTB.Clear();
+                numberOfYearAddTB.Clear();
+                //Добавить изминение размеров формы
+            }
+            if(tabControl1.SelectedIndex == 2)
+            {
+                LoadDataComboBox(comboBox1);
+            }
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (sqlConnection.State == ConnectionState.Closed) 
+                { 
+                    sqlConnection.Open(); 
+                }
+                if (!string.IsNullOrEmpty(nameAddTB.Text) && !string.IsNullOrWhiteSpace(nameAddTB.Text) &&
+                    !string.IsNullOrEmpty(numberOfYearAddTB.Text) && !string.IsNullOrWhiteSpace(numberOfYearAddTB.Text))
+                {
+                    SqlCommand command = new SqlCommand("INSERT INTO [Periodicals] (name, number_of_year)" +
+                        "VALUES(@name, @number_of_year)", sqlConnection);
+                    command.Parameters.AddWithValue("name", nameAddTB.Text);
+                    command.Parameters.AddWithValue("number_of_year", Convert.ToInt32(numberOfYearAddTB.Text));
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Запись успешно добавлена!");
+                }
+                else { MessageBox.Show("Все поля должны быть заполнены!"); }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), ex.Source.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (sqlConnection.State == ConnectionState.Open) 
+                { 
+                    sqlConnection.Close(); 
+                }
+            }
+        }
+
+        private void numberOfYearAddTB_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+            if (!Char.IsDigit(ch) && ch != 8)
+                e.Handled = true;
+        }
+
+        private void comboBox1_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            try
+            {
+                if (sqlConnection.State == ConnectionState.Closed)
+                {
+                    sqlConnection.Open();
+                }
+                SqlCommand command = new SqlCommand("SELECT * FROM [Periodicals] WHERE [id]=@id", sqlConnection);
+                command.Parameters.AddWithValue("id", comboBox1.SelectedValue);
+                SqlDataReader sqlReader = command.ExecuteReader();
+                sqlReader.Read();
+                nameUpdateTB.Text = Convert.ToString(sqlReader["name"]);
+                numberOfYearUpdateTB.Text = Convert.ToString(sqlReader["number_of_year"]);
+                sqlReader.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), ex.Source.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (sqlConnection.State == ConnectionState.Open) { sqlConnection.Close(); }
+            }
+        }
+
+        private void numberOfYearUpdateTB_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+            if (!Char.IsDigit(ch) && ch != 8)
+                e.Handled = true;
         }
     }
 }
